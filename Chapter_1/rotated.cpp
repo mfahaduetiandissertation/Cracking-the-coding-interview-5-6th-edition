3a26db4ece31b7e1e50f3d237fd2057995119d61
mfahadrobotics <mfahad@stevens.edu>
2019-08-30 17:07:24 -0400

    Update rotated.cpp

--- Chapter_1/rotated.cpp
+++ Chapter_1/rotated.cpp
@@ -5,13 +5,13 @@
 using namespace std;
 
 bool isRotated(string s1, string s2) {
-	int i1 = 1 ,i2 = s2.length()-1;
+	int i1 = 1;
 	string sb1, sb2, tstr;
 	if (s1.length() != s2.length())
 		return false;
 	while (i1 < s2.length()) {
-		tstr = s2.substr(i1, s2.length()-i1) + s2.substr(0, i1);
-		if (s1.compare(tstr)==0)
+		tstr = s2.substr(i1, s2.length() - i1) + s2.substr(0, i1);
+		if (s1.compare(tstr) == 0)
 			return true;
 		i1++;
 	}
@@ -27,9 +27,9 @@ int main() {
 	string str2 = "esttest";
 
 	if (isRotated(str1, str2))
-		cout << "String 2 is rotated version of String 1"<<endl;
+		cout << "String 2 is rotated version of String 1" << endl;
 	else
-		cout<< "String 2 is NOT rotated version of String 1" << endl;
+		cout << "String 2 is NOT rotated version of String 1" << endl;
 
 	return 0;
 
